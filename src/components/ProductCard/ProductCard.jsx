7a0353ca923a7ca77a8da9b09169c043de887758
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-09-07 01:47:17 +0600

    Products page design complate

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,27 @@
+import { TbShoppingBag } from "react-icons/tb";
+
+const ProductCard = () => {
+    return (
+        <div className="flex flex-col w-[277px] border border-[#F1F1F1] p-4 gap-8">
+            <div className="flex items-center justify-center bg-[#F2F2F2] w-[245px] h-[236px] rounded-lg mx-auto">
+                <img className="w-52 h-52" src="https://i.ibb.co.com/dPqbPXV/image-125-removebg-preview-1.png" alt="" />
+            </div>
+            <div className="flex flex-col gap-4 w-[245px]">
+                <h1 className="text-xl font-semibold text-[#343434]">Name</h1>
+
+                <div className="flex gap-3 justify-between items-center">
+                    <p className="text-xl font-bold text-[#343434]">$299.00</p>
+                    <p className="text-xl font-medium text-[#ABABAB] line-through">$350.00</p>
+                    <p className="text-xl font-semibold text-[#B92E2E]">30%OFF</p>
+                </div>
+                <h2 className="text-sm font-normal text-[#838383]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aut aliquid eaque blanditiis numquam non.</h2>
+            </div>
+            <button className="flex items-center justify-center gap-6 w-60 h-11 rounded bg-[#202020] text-[#ffffff] text-base font-semibold">
+                <TbShoppingBag className="text-xl" />
+                Add To cart
+            </button>
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 const Products = () => {
+
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
-            <h1> All Products Pages</h1>
+        <div className="flex md:w-[1450px]  mx-auto">
+            {/* menu section  */}
+            <div className="md:w-3/12 bg-[#E8E8E8] mx-auto justify-center">
+                <ul className="flex flex-col gap-4 p-3 w-56 mx-auto mt-4">
+                    <li className="text-2xl font-medium bg-[#0E0E0E] py-3 px-6 text-[#ffffff] rounded" >Rocking chair</li>
+                    <li className="text-2xl font-medium  py-3 px-6 rounded">Side chair</li>
+                    <li className="text-2xl font-medium  py-3 px-6 rounded">Lounge chair</li>
+                </ul>
+            </div>
+
+            {/* Product sections  */}
+            <div className="md:w-9/12  grid md:grid-cols-3 grid-cols-1 gap-3">
+                {products?.map((items) =>
+                    <ProductCard key={items.id} items={items} />
+                )}
+
+            </div>
         </div>
     );
 };
